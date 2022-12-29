d65972d481edd0347fef93ad980cf99a98152a1c
Sifat-Khan-cr <pskcrjr@gmail.com>
2022-12-29 22:58:24 +0600

    basic server created and mongodb connected

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express();
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dzhzyda.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+app.get('/', async (req, res) => {
+    res.send('doctors portal server is running')
+})
+
+app.listen(port, () => console.log(`Doctors portal running on ${port}`));
