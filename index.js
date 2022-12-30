e8b12a9382702ab5872d00100b3dc9e7c68b680e
Sifat-Khan-cr <pskcrjr@gmail.com>
2022-12-30 07:47:23 +0600

    bookings data management complete

--- index.js
+++ index.js
@@ -15,8 +15,63 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run() {
+    try {
+        const appointmentOptionCollection = client.db('Doctors-Portal').collection('appointmentOptions');
+        const bookingsCollection = client.db('Doctors-Portal').collection('bookingsCollection');
 
 
+
+        app.get('/appointmentOptions', async (req, res) => {
+            const date = req.query.date;
+            const query = {};
+            const options = await appointmentOptionCollection.find(query).toArray();
+            const bookingQuery = { selectedDate: date }
+            const booked = await bookingsCollection.find(bookingQuery).toArray();
+            options.forEach(option => {
+                const optionBooked = booked.filter(book => book.treatment === option.name)
+                const bookedSlots = optionBooked.map(book => book.slot)
+                const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
+                // console.log(bookedSlots, date, option.name, remainingSlots.length)
+                option.slots = remainingSlots;
+
+            })
+            res.send(options);
+        });
+
+
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const query = {
+                email: booking.email,
+                selectedDate: booking.selectedDate,
+                treatment: booking.treatment
+            }
+            const alreadyBooked = await bookingsCollection.find(query).toArray();
+
+            if (alreadyBooked.length) {
+                const message = `You have booking on ${booking.treatment} ${booking.selectedDate}`
+                return res.send({ acknowledged: false, message: message })
+            }
+
+
+
+
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+        })
+
+
+
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.log);
 
 
 app.get('/', async (req, res) => {
